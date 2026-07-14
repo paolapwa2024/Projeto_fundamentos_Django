452311b9d86eb39db0dce93951a69a32edcaaaac
paolapwa2024 <paola.pwa.2024@gmail.com>
2026-07-13 21:42:48 -0300

    fim da aula 13/07/2026

--- meusite/tarefas/urls.py
+++ meusite/tarefas/urls.py
@@ -5,4 +5,7 @@ app_name = 'tarefas'
 urlpatterns = [
     path('', views.tarefas_home, name='home'),
     path('adicionar/', views.tarefas_adicionar, name='adicionar'),
+    path('remover/<int:id>/', views.tarefas_remover, name='remover'),
+    path('editar/<int:id>/', views.tarefas_editar, name='editar')
+
 ]
--- meusite/tarefas/views.py
+++ meusite/tarefas/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import TarefaModel
 from .forms import TarefaForm
 # Create your views here.
 
 def tarefas_home(request):
     contexto = {
-        'nome': 'Bruno'
+        'nome': 'Bruno',
+        "tarefas": TarefaModel.objects.all()
     }
     return render(request, 'pagetarefas/home.html', contexto)
 
@@ -19,3 +21,22 @@ def tarefas_adicionar(request):
     }
 
     return render(request, 'pagetarefas/adicionar.html', contexto)
+
+def tarefas_remover(request, id):
+    tarefa = get_object_or_404(TarefaModel, id=id)
+    tarefa.delete()
+    return redirect('tarefas:home')
+
+def tarefas_editar(request, id):
+    tarefa = get_object_or_404(TarefaModel, id=id)
+    if request.method == 'POST':
+        formulario = TarefaForm(request.POST, instance=tarefa)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('tarefas:home')
+    else:
+        formulario = TarefaForm(instance=tarefa)
+    contexto = {
+        "form": formulario
+    }
+    return render(request, 'pagetarefas/editar.html', contexto)
